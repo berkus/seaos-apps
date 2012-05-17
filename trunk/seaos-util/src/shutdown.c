d29ef724b9c9966e54dc91bfef7fbf41a21b73fe
goldenpiranha <goldenpiranha@384710ce-13ac-ca34-ca1c-6334f9ef96f1>
2012-05-17 03:15:44 +0000

    overhauled startup and shutdown

--- trunk/seaos-util/src/shutdown.c
+++ trunk/seaos-util/src/shutdown.c
@@ -6,27 +6,7 @@
 #include <errno.h>
 #include <unistd.h>
 extern char **environ;
-int logout=1;
 int reset=0;
-void run_script_exact(char *path, int re)
-{
-	int pid;
-	if(!(pid=fork())) {
-		execl("/bin/sh", "sh", path, 0);
-		exit(255);
-	}
-	if(!re)
-		waitpid(pid, 0, 0);
-}
-
-void whoami(char *usr_name)
-{
-	struct passwd *pw = getpwuid(get_uid());
-	if(!pw)
-		return;
-	memset(usr_name, 0, 128);
-	strcpy(usr_name, pw->pw_name);
-}
 
 int main(int argc, char **argv)
 {
@@ -36,21 +16,15 @@ int main(int argc, char **argv)
 	{
 		if(!strcmp(argv[i], "-r"))
 			reset=1;
-		if(!strcmp(argv[i], "-l"))
-			logout=0;
 	}
 	i=0;
 	char sc[128];
 	char enter[128];
-	int qp;
-	if(!(qp = fork()))
-	{
-		if(reset)
-			execl("/bin/init", "init", "reset");
-		else
-			execl("/bin/init", "init", "down");
-	}
-	waitpid(qp, 0, 0);
+	system("sh /config/rc/shutdown");
+	if(reset) 
+		kernel_reset();
+	else
+		kernel_shutdown();
 	fprintf(stderr, "shutdown: Shutdown failed. You may need to be root\n");
 	return 0;
 }
