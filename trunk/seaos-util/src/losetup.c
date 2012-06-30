e82da369638872a344a891fca7dc31401e154fa5
goldenpiranha <goldenpiranha@384710ce-13ac-ca34-ca1c-6334f9ef96f1>
2012-06-30 01:36:39 +0000

    rewrote losetup

--- trunk/seaos-util/src/losetup.c
+++ trunk/seaos-util/src/losetup.c
@@ -5,38 +5,87 @@
 
 void usage()
 {
-	fprintf(stderr, "usage: ioctl [-d] loop-device file\n");
+	fprintf(stderr, "usage: losetup [-r] [-c | -x | -d] [-o offset] [-s size] loop_device [file]\n");
 }
 
 int main(int argc, char **argv)
 {
 	char *progname = (char *)basename(argv[0]);
-	int dis=0;
+	int dis=0, ro=0, create=0, remove=0;
 	int opt;
-	while((opt = getopt(argc, argv, "d")) != -1) {
+	char *file=0;
+	unsigned size=0, off=0;
+	while((opt = getopt(argc, argv, "cxrdo:s:")) != -1) {
 		switch(opt) {
 			case 'd':
 				dis=1;
 				break;
+			case 'r':
+				ro=1;
+				break;
+			case 'o':
+				off = atoi(optarg);
+				break;
+			case 's':
+				size = atoi(optarg);
+				break;
+			case 'c':
+				create=1;
+				break;
+			case 'x':
+				remove=1;
+				break;
 		}
 	}
-	if((optind + 2) > argc)
-	{
+	char *loop = argv[optind];
+	if(!loop) {
 		usage();
 		return 1;
 	}
-	FILE *f = fopen(argv[optind], "rw");
+	if(!dis && !create && !remove) {
+		file = argv[optind+1];
+		if(!file) {
+			usage();
+			return 1;
+		}
+	}
+	FILE *f = fopen(loop, "rw");
 	if(!f)
 	{
 		fprintf(stderr, "%s: could not open loop device '%s': %s\n", progname, argv[optind], strerror(errno));
 		return 2;
 	}
 	int ret;
-	if(dis)
-	{
+
+	if(create) {
+		ret = ioctl(fileno(f), 4, 0);
+	} else if(remove) {
+		ret = ioctl(fileno(f), 5, 0);
+	} else if(dis) {
 		ret = ioctl(fileno(f), 1, 0);
-	} else 
-		ret = ioctl(fileno(f), 0, (int)argv[optind+1]);
+	} else {
+		ret = ioctl(fileno(f), 0, (int)file);
+		if(ret) goto errors;
+		if(ro) {
+			if(ioctl(fileno(f), 6, 1)) {
+				fprintf(stderr, "%s: could not set loop device '%s' as read only: %s\n", progname, loop, strerror(errno));
+				ret=4;
+			}
+		}
+		if(size) {
+			if(ioctl(fileno(f), 3, size)) {
+				fprintf(stderr, "%s: could not set size on loop device '%s': %s\n", progname, loop, strerror(errno));
+				ret=5;
+			}
+		}
+		if(off) {
+			if(ioctl(fileno(f), 2, off)) {
+				fprintf(stderr, "%s: could not set offset on loop device '%s': %s\n", progname, loop, strerror(errno));
+				ret=6;
+			}
+		}
+	}
+	errors:
 	if(ret == -1)
 	{
 		switch(errno)
@@ -52,5 +101,5 @@ int main(int argc, char **argv)
 				break;
 		}
 	}
-	return ret;
+	return ret < 0 ? 3 : ret;
 }
