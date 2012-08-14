1c16629e976b101029ef4fc29c50fc2be00fe5b8
goldenpiranha <goldenpiranha@384710ce-13ac-ca34-ca1c-6334f9ef96f1>
2012-08-14 09:14:05 +0000

    updated gcc to 4.7.1

--- porting/ported/files/seaos.h
+++ porting/ported/files/seaos.h
@@ -1,8 +1,9 @@
   #undef TARGET_OS_CPP_BUILTINS
   #define TARGET_OS_CPP_BUILTINS()      \
     do {                                \
-      builtin_define_std ("myos");      \
+      builtin_define_std ("seaos");      \
       builtin_define_std ("unix");      \
-      builtin_assert ("system=myos");   \
+      builtin_assert ("system=seaos");   \
       builtin_assert ("system=unix");   \
     } while(0);
+
--- porting/ported/patch_gcc.sh
+++ porting/ported/patch_gcc.sh
@@ -1,5 +1,8 @@
-wget http://ftp.gnu.org/gnu/gcc/gcc-4.7.0/gcc-4.7.0.tar.gz
-tar xzf gcc-4.7.0.tar.gz
-cd gcc-4.7.0
-patch -p1 -i ../patches/gcc-4.7.0-seaos.patch
+wget http://ftp.gnu.org/gnu/gcc/gcc-4.7.1/gcc-4.7.1.tar.gz
+tar xzf gcc-4.7.1.tar.gz
+cd gcc-4.7.1
+patch -p1 -i ../patches/gcc-4.7.1-seaos.patch
 cp ../files/seaos.h gcc/config/
+cd libstdc++-v3
+autoconf
+cd ..
