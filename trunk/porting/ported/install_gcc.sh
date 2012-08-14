a907dcba9f9646790cf67128a53a41e0a7a39cd8
goldenpiranha <goldenpiranha@384710ce-13ac-ca34-ca1c-6334f9ef96f1>
2012-08-14 09:14:05 +0000

    updated gcc to 4.7.1

--- trunk/porting/ported/install_gcc.sh
+++ trunk/porting/ported/install_gcc.sh
@@ -1,5 +1,5 @@
-cd gcc-4.7.0
-./configure --disable-ssp --host=i586-pc-seaos --prefix=/usr --disable-nls --enable-languages=c
+cd gcc-4.7.1
+./configure --host=i586-pc-seaos --prefix=/usr --disable-nls --enable-languages=c,c++ CC_FOR_TARGET=i586-pc-seaos-gcc AR_FOR_TARGET=i586-pc-seaos-ar AS_FOR_TARGET=i586-pc-seaos-as RANLIB_FOR_TARGET=i586-pc-seaos-ranlib STRIP_FOR_TARGET=i586-pc-seaos-strip
 
 for i in `find -name "config.h"`; do
 	echo -e "#undef HAVE_MMAP\n#undef HAVE_MMAP_ANON\n#undef HAVE_MMAP_DEV_ZERO\n#undef HAVE_MMAP_FILE\n" >> $i
@@ -9,5 +9,7 @@ for i in `find -name "auto-host.h"`; do
 	echo -e "#undef HAVE_MMAP\n#undef HAVE_MMAP_ANON\n#undef HAVE_MMAP_DEV_ZERO\n#undef HAVE_MMAP_FILE\n" >> $i
 done
 
-make all-gcc $MTHREAD
-make install-gcc DESTDIR="`pwd`/../../../data/" 
+make all-gcc $MTHREAD  CC_FOR_TARGET=i586-pc-seaos-gcc AR_FOR_TARGET=i586-pc-seaos-ar AS_FOR_TARGET=i586-pc-seaos-as RANLIB_FOR_TARGET=i586-pc-seaos-ranlib STRIP_FOR_TARGET=i586-pc-seaos-strip
+make install-gcc DESTDIR="`pwd`/../../../data/"   CC_FOR_TARGET=i586-pc-seaos-gcc AR_FOR_TARGET=i586-pc-seaos-ar AS_FOR_TARGET=i586-pc-seaos-as RANLIB_FOR_TARGET=i586-pc-seaos-ranlib STRIP_FOR_TARGET=i586-pc-seaos-strip
+make all-target-libgcc  CC_FOR_TARGET=i586-pc-seaos-gcc AR_FOR_TARGET=i586-pc-seaos-ar AS_FOR_TARGET=i586-pc-seaos-as RANLIB_FOR_TARGET=i586-pc-seaos-ranlib STRIP_FOR_TARGET=i586-pc-seaos-strip
+make install-target-libgcc DESTDIR="`pwd`/../../../data/"  CC_FOR_TARGET=i586-pc-seaos-gcc AR_FOR_TARGET=i586-pc-seaos-ar AS_FOR_TARGET=i586-pc-seaos-as RANLIB_FOR_TARGET=i586-pc-seaos-ranlib STRIP_FOR_TARGET=i586-pc-seaos-strip
